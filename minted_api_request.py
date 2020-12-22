c52bae09d2d50bbb5c07c64790cdf649a959c7b5
Will Karnasiewicz <will.karnasiewicz@boulevardcg.com>
2020-12-22 10:07:41 -0500

    refactor because selenium find_element_by_* is deprecated

--- minted_api_request.py
+++ minted_api_request.py
@@ -5,6 +5,7 @@ import pandas as pd
 import requests
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Webdriver options; set to headless
@@ -29,11 +30,11 @@ except KeyError:
 driver.get(URL)
 
 # Selenium deals with lgin form
-email_elem = driver.find_element_by_name("email")
+email_elem = driver.find_element(By.NAME, "email")
 email_elem.send_keys(minted_email)
-password_elem = driver.find_element_by_name("password")
+password_elem = driver.find_element(By.NAME, "password")
 password_elem.send_keys(minted_password)
-login_submit = driver.find_element_by_class_name("loginButton")
+login_submit = driver.find_element(By.CLASS_NAME, "loginButton")
 login_submit.click()
 
 sleep(5)  # to load JS and be nice
