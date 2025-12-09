3b98e807b2bbbaf89c3d79b0ac22d9c9405cb1e8
Will Karnasiewicz <willkarnasiewicz@gmail.com>
2025-12-09 05:14:41 -0500

    fix pre-commit workflow:

--- minted_api_request.py
+++ minted_api_request.py
@@ -5,13 +5,15 @@ import pandas as pd
 import requests
 from selenium.webdriver import Chrome
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
 # Webdriver options; set to headless
 options = Options()
 options.add_argument("--headless")
-driver = Chrome(executable_path=ChromeDriverManager().install(), options=options)
+service = Service(ChromeDriverManager().install())
+driver = Chrome(service=service, options=options)
 
 # URL for minted login page
 URL = "https://www.minted.com/login"
@@ -30,12 +32,12 @@ except KeyError:
 driver.get(URL)
 
 # Selenium deals with lgin form
-email_elem = driver.find_element(By.NAME, "email")
+email_elem = driver.find_element(By.XPATH, '//*[@id="identifierMNTD"]')
 email_elem.send_keys(minted_email)
-password_elem = driver.find_element(By.NAME, "password")
+password_elem = driver.find_element(By.XPATH, '//*[@id="password"]')
 password_elem.send_keys(minted_password)
 login_submit = driver.find_element(
-    By.XPATH, "/html/body/div/div[3]/div/form/div[2]/button[1]"
+    By.XPATH, '//*[@id="__next"]/div[3]/div/form/div[2]/div[1]/button'
 )
 login_submit.click()
 
@@ -46,7 +48,9 @@ cookies = {c["name"]: c["value"] for c in driver.get_cookies()}
 
 # Request address book contents as json
 response = requests.get(
-    "https://addressbook.minted.com/api/contacts/contacts/?format=json", cookies=cookies
+    "https://addressbook.minted.com/api/contacts/contacts/?format=json",
+    cookies=cookies,
+    timeout=300,
 )
 listings = response.json()
 
